4bd49afa9c115e1dca6659b907ec86dfda81dfc9
Semere Tereffe <se.semere.tereffe@gmail.com>
2022-05-01 19:35:18 +0300

    Complete Problem

---
+++ a2sv-comminity-progress/remove-duplicates-from-sorted-list.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    # def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def deleteDuplicates(self, head):
+        if not head.next: return head
+        currentPointer = head
+        while currentPointer and currentPointer.next:
+            if currentPointer.val == currentPointer.next.val:
+                currentPointer.next = currentPointer.next.next
+            else:
+                currentPointer = currentPointer.next
+        

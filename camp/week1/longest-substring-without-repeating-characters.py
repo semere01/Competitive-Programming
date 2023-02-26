f09313043c1da4316bb4e2e0d2bad185d7bd2e08
semere01 <se.semere.tereffe@gmail.com>
2023-02-26 05:24:31 +0300

    Update longest-substring-without-repeating-characters.py

--- camp/week1/longest-substring-without-repeating-characters.py
+++ camp/week1/longest-substring-without-repeating-characters.py
@@ -1,27 +1,12 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if (len(s) < 2):
-            return (len(s))
-        s = list(s)
-
-        l = 0
-        r = 0
-        largest = 0
-        memSet = set()
-        while (r < len(s)):
-            if (s[r] not in memSet):
-                memSet.add(s[r])
-            else:
-                if ( largest < len(memSet)):
-                    largest = len(memSet)
-                memSet.remove(s[r])
-                while (s[l] != s[r]):
-                    if (s[l] in memSet): memSet.remove(s[l])
-                    l += 1
-                l += 1
-                memSet.add(s[l])
-            r += 1
+        substr = ''
+        max_len = 0
         
-        return largest
-
-print(Solution().lengthOfLongestSubstring("pwwkew"))
+        for char in s:
+            while char in substr:
+                substr = substr[1:]
+            substr += char
+            max_len = max(max_len, len(substr))
+        
+        return max_len
